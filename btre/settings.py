05f17dee16d7f5314e4d9e0a2e438bb48053a71e
aastham-125 <aastha.mahindra125@gmail.com>
2021-06-19 02:38:11 +0530

    Initial commit

--- btre/settings.py
+++ btre/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-ljvt)exu*w3v@a=#3ez-2e^yu37$=o7_)%#)(=wjl5xu9zfum%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
