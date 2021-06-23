3eae2b0e7bef106e739d39622f973691d7d0c2a1
aastham-125 <aastha.mahindra125@gmail.com>
2021-06-23 05:34:25 +0530

    Admin Setup

--- btre/urls.py
+++ btre/urls.py
@@ -14,15 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('listings/',include('listings.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)# SECURITY WARNING: keep the secret key used in production secret!
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ljvt)exu*w3v@a=#3ez-2e^yu37$=o7_)%#)(=wjl5xu9zfum%'
