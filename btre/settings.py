4d1c01f5396a24151116c375c9df1b5d7ada4d7a
aastham-125 <aastha.mahindra125@gmail.com>
2021-06-28 16:55:29 +0530

    Send E-mail and Dynamic Dashboard

--- btre/settings.py
+++ btre/settings.py
@@ -145,3 +145,10 @@ MESSAGE_TAG = {
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# email config
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT =587
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
